64d162d61cfe48d3037122afff51b05a471587d8
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-02-11 22:51:16 -0500

    feat: movement of island trough drag event and plane animation added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,29 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "./Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+import { useState } from "react";
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     const screenPosition = [0, -6.5, -43.4];
@@ -21,19 +42,35 @@ const Home = () => {
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
 
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
   return (
     <section className="w-full h-screen">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`
+        w-full h-screen bg-transparent
+        ${isRotating ? "cursor-grabbing" : "cursor-grab"}
+        `}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={3} />
           <ambientLight intensity={0.5} />
+          <Sky />
+          <Bird />
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+            isRotating={isRotating}
+          />
           <Island
             position={islandPosition}
             islandScale={islandScale}
             islandRotation={islandRotation}
+            isRotating={isRotating}
+            setCurrentStage={setCurrentStage}
+            setIsRotating={setIsRotating}
           />
           <hemisphereLight
             skyColor="#b1e1ff"
