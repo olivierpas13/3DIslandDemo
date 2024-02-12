64816c10f528eb825adb416d44b5656e70c04ca9
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-02-11 20:57:04 -0500

    feat: Island model added with its lightning

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,46 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "./Loader";
+import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    const screenPosition = [0, -6.5, -43.4];
+    const rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>} ></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={3} />
+          <ambientLight intensity={0.5} />
+          <Island
+            position={islandPosition}
+            islandScale={islandScale}
+            islandRotation={islandRotation}
+          />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+        </Suspense>
       </Canvas>
     </section>
   );
